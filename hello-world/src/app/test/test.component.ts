eb164f6e2e0ba85b7551a890b6b5eabcf265628e
Ray3110 <ray3110@live.hk>
2022-06-14 17:37:55 +0800

    Components

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -1,25 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // Customized html header
+  // Customized html tag selector
   // selector: 'app-test',
   // Class selector
   // selector: '.app-test',
   // Attribute selector
-  selector: '[app-test]',
+    selector: '[app-test]',
   // templateUrl: './test.component.html',
   // Inline html template
-  template: `<div>
-                  Inline template
-                </div>`,
+    template: `<div>
+                    Inline template
+              </div>`,
   // styleUrls: ['./test.component.css']
   // Inline style
-  styles: [`
-    div {
-      colour: red;
-      }`
-  ]
-
+    styles: [`
+      div {
+        color: red;
+        }`
+    ]
 })
 export class TestComponent implements OnInit {
 
