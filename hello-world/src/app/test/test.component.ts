79cfc5fdac9f58796f3d50bb2a719521f39a605d
Ray3110 <ray3110@live.hk>
2022-06-14 18:16:51 +0800

    Interpolation

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -10,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
   // templateUrl: './test.component.html',
   // Inline html template
     template: `<div>
-                    Inline template
+                  <h2>
+                    Welcome {{name}}
+                  </h2>
+                  <h2>{{2+2}}</h2>
+                  <h2>{{"Hello " +name}}</h2>
+                  <h2>{{name.length}}</h2>
+                  <h2>{{name.toUpperCase()}}</h2>
+                  <h2>{{greetUser()}}</h2>
+                  <h2>{{siteUrl}}</h2>
               </div>`,
   // styleUrls: ['./test.component.css']
   // Inline style
@@ -22,9 +30,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+// Interporation
+  // Class property
+  public name = "Ray";
+  // Access JS global variable
+  public siteUrl = window.location.href;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetUser() {
+    return "Hi " + this.name;
   }
 
 }
