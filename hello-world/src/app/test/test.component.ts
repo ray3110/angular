3985e3f7587108d76a9abc7b503e8d129b31a1eb
Ray Lee <ray3110@live.hk>
2022-06-14 22:49:08 +0800

    Property binding

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -1,48 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // Customized html tag selector
-  // selector: 'app-test',
-  // Class selector
-  // selector: '.app-test',
-  // Attribute selector
     selector: '[app-test]',
-  // templateUrl: './test.component.html',
-  // Inline html template
-    template: `<div>
-                  <h2>
-                    Welcome {{name}}
-                  </h2>
-                  <h2>{{2+2}}</h2>
-                  <h2>{{"Hello " +name}}</h2>
-                  <h2>{{name.length}}</h2>
-                  <h2>{{name.toUpperCase()}}</h2>
-                  <h2>{{greetUser()}}</h2>
-                  <h2>{{siteUrl}}</h2>
-              </div>`,
-  // styleUrls: ['./test.component.css']
-  // Inline style
-    styles: [`
-      div {
-        color: red;
-        }`
-    ]
+    template: `
+      <h2>
+        Welcome {{name}}
+      </h2>
+      <input [id]="myId" type="text" value="Ray">
+      // Property binding
+      // <input [disabled]="isDisabled" [id]="myId" type="text" value="Ray">
+      // Alternative representation
+      <input bind-disabled="isDisabled" [id]="myId" type="text" value="Ray">
+      `,
+    styles: []
 })
 export class TestComponent implements OnInit {
 
 // Interporation
   // Class property
   public name = "Ray";
-  // Access JS global variable
-  public siteUrl = window.location.href;
+  public myId = "testId"
+  public isDisabled = false
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  greetUser() {
-    return "Hi " + this.name;
   }
 
 }
